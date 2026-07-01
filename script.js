98dc62819c7dbf16063908ac55c299d49485f397
ANKITpandey75 <125044396+ANKITpandey75@users.noreply.github.com>
2026-07-01 13:52:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-//your JS code here. If required.
+const codes = document.querySelectorAll(".code");
+
+codes[0].focus();
+
+codes.forEach((code, index) => {
+
+    code.addEventListener("input", (e) => {
+
+        code.value = e.target.value.replace(/[^0-9]/g, "");
+
+        if (code.value !== "" && index < codes.length - 1) {
+            codes[index + 1].focus();
+        }
+
+    });
+
+    code.addEventListener("keydown", (e) => {
+
+        if (e.key === "Backspace") {
+
+            if (code.value === "" && index > 0) {
+                codes[index - 1].value = "";
+                codes[index - 1].focus();
+            } else {
+                code.value = "";
+            }
+
+        }
+
+    });
+
+});//your JS code here. If required.
